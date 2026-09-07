2f4c1f61274386a5fc5dbba554379758cd83bafc
tetragrammalol <[TOKEN_EXPIRED]>
2026-09-07 15:57:20 +0000

    Restore missing android.util.Log import after adopt merge

--- app/src/main/java/com/samourai/sentinel/ui/tools/sweep/SweepPrivKeyFragment.kt
+++ app/src/main/java/com/samourai/sentinel/ui/tools/sweep/SweepPrivKeyFragment.kt
@@ -8,6 +8,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
